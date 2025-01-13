088fd8fe4d56c252b42d5e0ee76d0fc49ab277d1
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2025-01-13 11:02:25 +0000

    [JAN,13] - tuples against objects

--- Typescript/features/tuples.ts
+++ Typescript/features/tuples.ts
@@ -4,4 +4,14 @@ const drink = {
   sugar: 40
 }
 
-const pepsi:[string, boolean, number] = ['red', true,40];
+type Drink = [string, boolean, number];
+const pepsi:Drink = ['red', true,40];
+const sprite: Drink = ['clear', true, 40];
+const tea: Drink = ['brown', false, 0];
+
+const carSpecs: [number, number] = [400, 3354];
+const carStats = {
+  horsepower: 400,
+  weight: 3354
+};
+
