d308a16c8e1f6fe7fe8a1c1453355ab426890d4c
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2025-01-13 11:14:24 +0000

    [JAN,13] - funciton definition inside interface

--- Typescript/features/interfaces.ts
+++ Typescript/features/interfaces.ts
@@ -1,13 +1,17 @@
 interface Vehicle {
   name: string;
-  year: number;
+  year: Date;
   broken: boolean;
+  summary():string;
 }
 
 const oldCivic = {
   name: 'civic',
-  year: 2000,
-  broken: true
+  year: new Date(),
+  broken: true,
+  summary():string {
+    return `Name: ${this.name}`;
+  }
 };
 
 const printVehicle = (vehicle: Vehicle ):void => {
