81ac68d7aca58f54ae04b3b2762daf308adb8812
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2025-01-13 09:43:16 +0000

    [JAN,13] - function object argument with destructuring and type annotation

--- Typescript/features/functions.ts
+++ Typescript/features/functions.ts
@@ -27,9 +27,9 @@ const todayWeather = {
   weather: 'sunny'
 }
 
-const logWeather = (forecast: {date: Date, weather:string}):void => {
-  console.log(forecast.date);
-  console.log(forecast.weather);
+const logWeather = ({date,weather}: {date: Date, weather:string}):void => {
+  console.log(date);
+  console.log(weather);
 }
 
 logWeather(todayWeather);
