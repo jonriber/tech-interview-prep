8e5fa533d4f18814c4d9c8a1290f725f062d6473
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2025-01-13 11:21:27 +0000

    [JAN,13] - Refactoring to a more generic interface | reusable code in typescript

--- Typescript/features/interfaces.ts
+++ Typescript/features/interfaces.ts
@@ -1,7 +1,4 @@
-interface Vehicle {
-  name: string;
-  year: Date;
-  broken: boolean;
+interface Reportable {
   summary():string;
 }
 
@@ -14,10 +11,17 @@ const oldCivic = {
   }
 };
 
-const printVehicle = (vehicle: Vehicle ):void => {
-  console.log(`Name: ${vehicle.name}`);
-  console.log(`Year: ${vehicle.year}`);
-  console.log(`Broken: ${vehicle.broken}`);
+const drink1 = {
+  color: 'red',
+  carbonated: true,
+  sugar: 40,
+  summary():string {
+    return `My drink has ${this.sugar} grams of sugar`;
+  }
+};
+
+const printSummary = (item: Reportable ):void => {
+  console.log(item.summary());
 }
 
-printVehicle(oldCivic);
+printSummary(oldCivic);
