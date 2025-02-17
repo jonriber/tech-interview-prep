be5bd0598a3a306358765a17cbe4607a739c8fec
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2025-02-17 09:04:26 +0000

    [FEB,17] - dynamic array solving structure

--- HackerRank/ProblemSolving/dynamicArray/dynamic_array.py
+++ HackerRank/ProblemSolving/dynamicArray/dynamic_array.py
@@ -1,6 +1,14 @@
 
 def dynamicArray(n, queries):
-  pass
+  lastAnswer = 0
+  sequences = [[] for _ in range(n)]
+  for _ in range(queries):
+    print("Query:",_)
+    
+  
 
 if (__name__== '__main__'):
-  pass
+  print("Starting Dynamic Array problem")
+  dynamicArray(2, 5)
+  
+  
