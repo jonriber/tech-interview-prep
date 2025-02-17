37ff22d2d2e3d3774c14fbdf11d6f47502256d3e
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2025-02-17 09:04:48 +0000

    [FEB,17] - Next.js to-do study

--- Next.js/PoCs/ToDo/app/page.tsx
+++ Next.js/PoCs/ToDo/app/page.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import Image from 'next/image';
+import buildPic from '../public/build.png';
 export default function Page() {
   return(
     <>
       <h1>Next.js App</h1>
+      <Image src={buildPic} alt="Building pic"/>
     </>
   )
 }
