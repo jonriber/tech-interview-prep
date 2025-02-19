307d92d6ad1c6ca0a8faec6ed476ca41e4948705
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2025-02-19 10:44:09 +0000

    [FEB,19] - Remote and local images & importing fonts in the layout file

--- Next.js/PoCs/ToDo/app/page.tsx
+++ Next.js/PoCs/ToDo/app/page.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import Image from 'next/image';
 import buildPic from '../public/build.png';
 export default function Page() {
+  let showExternalImg = false;
   return(
     <>
       <h1>Next.js App</h1>
+      {/* {showExternalImg && <Image src="https://via.placeholder.com/150" alt="Placeholder" width={500} height={500}/>} */}
       <Image src={buildPic} alt="Building pic"/>
     </>
   )
