9cfeac8a5d6a4d4cf29ea62e90b22ea603df677f
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2025-02-20 06:14:30 +0000

    [FEB,20] - Fetching data on server side components

--- Next.js/PoCs/ToDo/app/blog/page.tsx
+++ Next.js/PoCs/ToDo/app/blog/page.tsx
@@ -1,15 +1,17 @@
 import React from "react"
-import { getPosts } from '@/lib/posts';
+// import { getPosts } from '@/lib/posts';
 import Post  from "@/ui/post";
 import styles from './styles.module.css';
 
 export default async function Page() {
-  const posts = await getPosts();
+  const data = await fetch('https://api.vercel.app/blog')
+  const posts = await data.json()
 
   return (
     <ul className={styles.blog}>
       {posts.map((post) => (
-        <Post key={post.id} post={post} />
+        // <Post key={post.id} post={post} />
+        <li key={post.id}>{post.title}</li>
       ))}
     </ul>
   )
