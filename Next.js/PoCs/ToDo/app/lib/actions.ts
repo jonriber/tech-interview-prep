dc4717593cbf18bb9e8c95ac2f60db412ddbfacb
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2025-02-20 06:33:05 +0000

    [FEB,20] - formData on server actions & Forms server side component

--- Next.js/PoCs/ToDo/app/lib/actions.ts
+++ Next.js/PoCs/ToDo/app/lib/actions.ts
@@ -1,6 +1,15 @@
 'use server';
 
-export async function createPost(formData: FormData) {};
+export async function createPost(formData: FormData) {
+  console.log('This is a server function createPost');
+  const title = formData.get('title');
+  const content = formData.get('content');
+
+  // update data on the daba base
+  // revalidade cache
+  // return new data
+  
+};
 
 export async function deletePost(formData:FormData){};
 
