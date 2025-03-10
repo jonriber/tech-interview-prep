35e218d5efbcec6a42f6a81e2e303cd3b2948ddf
jonatas Ribeiro <jonatas@prottaribeiro.com>
2025-03-10 19:23:16 +0000

    [MAR,10] - create a new book CRUD ROUTE

--- API/Practice/Python/FastAPI/Book Review API/main.py
+++ API/Practice/Python/FastAPI/Book Review API/main.py
@@ -1,4 +1,14 @@
 from fastapi import FastAPI
+from models import Review, Book
+
+
+## Data base Mock 
+
+books_db = [
+  {"id": 1, "title": "1984", "author": "George Orwell", "description": "Dystopian novel.", "reviews": []},
+  {"id": 2, "title": "To Kill a Mockingbird", "author": "Harper Lee", "description": "Classic novel.", "reviews": []}
+]
+
 
 app = FastAPI()
 
@@ -6,3 +16,13 @@ app = FastAPI()
 def read_root():
   return {"message": "Welcome to the Book Review API"}
 
+## Defining my CRUD ROUTES
+
+
+## Create new book using the POST method
+@app.post("/books/")
+def create_book(book:Book):
+  books_db.append(book.dict())
+  return books_db[-1]
+
+## 
