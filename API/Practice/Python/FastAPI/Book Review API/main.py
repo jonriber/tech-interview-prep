66d674a7e721c8d24c2e3a2ef257d286065cc3e4
jonatas Ribeiro <jonatas@prottaribeiro.com>
2025-03-10 22:27:22 +0000

    [MAR,10] - crud routes for book and reviews of a book

--- API/Practice/Python/FastAPI/Book Review API/main.py
+++ API/Practice/Python/FastAPI/Book Review API/main.py
@@ -38,3 +38,64 @@ def get_book(book_id: int):
       return book
   return {"error": "Book not found"}
 
+## Delete a book using the DELETE method
+@app.delete("/books/{book_id}")
+def delete_book(book_id: int):
+  for book in books_db:
+    if book["id"] == book_id:
+      books_db.remove(book)
+      return {"message": "Book deleted successfully"}
+  return {"error": "Book not found"}
+    
+## Update a book using the PUT method
+@app.put("/books/{book_id}")
+def update_book(book_id: int, book: Book):
+  for book in books_db:
+    if book["id"] == book_id:
+      books_db[book_id] = book.dict()
+      return {"message": "Book updated successfully"}
+  return {"error": "Book not found"}
+
+## ROutes for reviews
+
+## create a review for a book using the POST method
+@app.post("/books/{book_id}/reviews/")
+def create_review(book_id: int, review: Review):
+  for book in books_db:
+    if book["id"] == book_id:
+      book["reviews"].append(review.dict())
+      return book
+  return {"error": "Book not found"}
+
+## Read all reviews for a book using the GET method
+@app.get("/books/{book_id}/reviews/")
+def get_reviews(book_id: int):
+  for book in books_db:
+    if book["id"] == book_id:
+      return book["reviews"]
+  return {"error": "Book not found"}
+
+## update an specific review for a book using the PUT method
+@app.put("/books/{book_id}/reviews/{review_id}")
+def update_review(book_id: int, review_id: int, review: Review):
+  for book in books_db:
+    if book["id"] == book_id:
+      for review in book["reviews"]:
+        if review["id"] == review_id:
+          books_db[book_id]["reviews"][review_id] = review.dict()
+          return {"message": "Review updated successfully"}
+      return {"error": "Review not found"}
+  return {"error": "Book not found"}
+
+## delete a review for a book using the DELETE method
+
+@app.delete("/books/{book_id}/reviews/{review_id}")
+def delete_review(book_id: int, review_id: int):
+  for book in books_db:
+    if book["id"] == book_id:
+      for review in book["reviews"]:
+        if review["id"] == review_id:
+          book["reviews"].remove(review)
+          return {"message": "Review deleted successfully"}
+      return {"error": "Review not found"}
+  return {"error": "Book not found"}
