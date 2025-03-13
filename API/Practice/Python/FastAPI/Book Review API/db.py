d2c5a6b6d95d18b1d5999c0bb27d0f9f581b5a3e
jonatas Ribeiro <jonatas@prottaribeiro.com>
2025-03-13 00:23:16 +0000

    [MAR,13] - creating a real postgreSQL connection

--- API/Practice/Python/FastAPI/Book Review API/db.py
+++ API/Practice/Python/FastAPI/Book Review API/db.py
@@ -1,4 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 from models import Book, Review
+
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@localhost/book_review_db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+base = declarative_base()
 
 def initialize_db(database):
   database.connect()
